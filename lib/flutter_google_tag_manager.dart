f4948bb4cfae36cc21b4163f72ae5983ef82c682
Herbert Poul <herbert@codeux.design>
2020-02-29 17:53:37 +0100

    add noop implementation for non-web builds.

---
+++ lib/flutter_google_tag_manager.dart
@@ -0,0 +1,25 @@
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+abstract class FlutterGoogleTagManager extends PlatformInterface {
+  FlutterGoogleTagManager() : super(token: _token);
+
+  static final Object _token = Object();
+
+  static FlutterGoogleTagManager _instance = FlutterGoogleTagManagerNoop();
+
+  static FlutterGoogleTagManager get instance => _instance;
+
+  /// Platform-specific plugins should set this with their own platform-specific
+  /// class that extends [UrlLauncherPlatform] when they register themselves.
+  static set instance(FlutterGoogleTagManager instance) {
+    PlatformInterface.verifyToken(instance, _token);
+    _instance = instance;
+  }
+
+  void pushEvent(String eventName, {Map<String, dynamic> args});
+}
+
+class FlutterGoogleTagManagerNoop extends FlutterGoogleTagManager {
+  @override
+  void pushEvent(String eventName, {Map<String, dynamic> args}) {}
+}
--- lib/flutter_google_tag_manager_web.dart
+++ lib/flutter_google_tag_manager_web.dart
@@ -3,6 +3,7 @@ library flutter_google_tag_manager_web;
 
 import 'dart:async';
 
+import 'package:flutter_google_tag_manager_web/flutter_google_tag_manager.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:flutter/services.dart';
 import 'package:js/js.dart';
@@ -15,18 +16,14 @@ void _dataLayerPush(Map<String, dynamic> data) {
   _push(jsify(data));
 }
 
-class FlutterGoogleTagManagerWebPlugin {
-  FlutterGoogleTagManagerWebPlugin._();
-  factory FlutterGoogleTagManagerWebPlugin() => _instance;
-
-  static FlutterGoogleTagManagerWebPlugin _instance;
-
+class FlutterGoogleTagManagerWebPlugin extends FlutterGoogleTagManager {
   void pushEvent(String eventName, {Map<String, dynamic> args}) {
     _dataLayerPush({'event': eventName, ...?args});
   }
 
   /// Registers that [FirebaseCoreWeb] is the platform implementation.
   static void registerWith(Registrar registrar) {
-    _instance = FlutterGoogleTagManagerWebPlugin._();
+//    _instance = FlutterGoogleTagManagerWebPlugin._();
+    FlutterGoogleTagManager.instance = FlutterGoogleTagManagerWebPlugin();
   }
 }
